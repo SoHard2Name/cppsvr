def28185c768155210dd12354b24d75ab3b9c531
abcpony <2490273063@qq.com>
2024-06-22 10:50:02 -0700

    备份。

--- test/testlogger.cpp
+++ test/testlogger.cpp
@@ -1,11 +1,17 @@
 #include "iostream"
 #include "utils/logger.h"
+#include "utils/commfunctions.h"
 
 // 测试同步 logger
 
 int main() {
-	
-	
-	
+	DEBUG("begin main()");
+	for (int i = 0; i < 5; i++) {
+		INFO("succ %d", i);
+		std::cout << utility::StrFormat("succ %d", i) << " ...... \n";
+	}
+	std::string sErrMsg = "damn";
+	ERROR("%s, man", sErrMsg.c_str());
+
 	return 0;
 }
